1acbddb44643546623cba0833d664945a57510c8
midhun kp <mithula@adamscreation.com>
2021-01-11 12:19:42 +0530

    First commit

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1592219225;s:7:"authKey";s:64:"J9(I#uKFH G~<j7Vg;/Ohw2!7k$>rQD)NgqU6p(^O{hz?bm*@}/T/pq.(j5Gm2G*";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:1336;}
